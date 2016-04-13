d5c63f73d9345bd8c744f14a195ec48017103743
Nic Smith <48gnomes@gmail.com>
2016-04-13 17:56:54 -0400

    Make a hashtable

---
+++ cis237hashtable/HashMap.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237hashtable
+{
+    class HashMap
+    {
+        //Make a prime value to use in the hash function, and as the size 
+        //of the array.
+        private const int PRIMEVALUE = 97;
+        //Define a few arrays.  One for keys, one for values
+        int[] theKeys;
+        string[] theValues;
+
+        //Constructor
+        public HashMap()
+        {
+            theKeys = new int[PRIMEVALUE];
+            theValues = new string[PRIMEVALUE];
+        }
+
+        public override string ToString()
+        {
+            string returnString = "Key     => value\n";
+            returnString += "-----------------------------------------------------\n";
+            for (int i = 0; i< theKeys.Length; i++)
+            {
+                if(theKeys[i] != 0)
+                {
+                    returnString += theKeys[i] + "  => " + theValues[i] + "\n";
+                }
+            }
+            return returnString;
+        }
+
+        public string ShowArrays()
+        {
+            string returnString = "Idx - Key    => Value\n";
+            returnString += "--------------------------------\n";
+            for(int i = 0; i < theKeys.Length; i++)
+            {
+                returnString += "[" + i + "] - " + theKeys[i] + " => " + theValues[i] + "/n" + Environment.NewLine;
+            }
+            return returnString;
+        }
+
+        public void Put(int Key, string Value)
+        {
+            //Get the index to use for storing the info from the hash function
+            int indexForValue = HashFunction(Key);
+            //use the index to put the key and value into the associated array.
+            theKeys[indexForValue] = Key;
+            theValues[indexForValue] = Value;
+        }
+
+        public string Get(int Key)
+        {
+            //Get the index to use for looking up the value
+            int indexForValues = HashFunction(Key);
+            //return the information at the array location of indexForValues
+            return theValues[indexForValues];
+        }
+
+        //Hash function to take in a key and map it to a smaller number space that
+        //is between 0 and PRIMEVALUE.  This is accomplished by the MOD function.
+        private int HashFunction(int key)
+        {
+            return key % PRIMEVALUE;
+        }
+    }
+}
--- cis237hashtable/Program.cs
+++ cis237hashtable/Program.cs
@@ -10,6 +10,49 @@ namespace cis237hashtable
     {
         static void Main(string[] args)
         {
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>();
+            
+            HashMap myHashMap = new HashMap();
+
+            Console.SetBufferSize(80, 1200);
+
+            Console.WriteLine("Adding some entries to the HashMap");
+            Console.WriteLine("Ex: valleyNumToName.Put(12345, \"David Barnes\" ");
+            myHashMap.Put(12345, "Thane");
+            myHashMap.Put(23456, "Jon Snow");
+            myHashMap.Put(34567, "Mother Theresa");
+            myHashMap.Put(45678, "Varric Tethras");
+            myHashMap.Put(56789, "Steve Rogers");
+
+            Console.WriteLine("The created hash table");
+            Console.WriteLine(myHashMap.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("The full arrays as they got filled out");
+            Console.WriteLine(myHashMap.ShowArrays());
+            Console.WriteLine();
+
+            Console.WriteLine("*******************************************");
+
+            Console.WriteLine("Get a single value out from the hash table passing in the key");
+            Console.WriteLine("myHashMap.Get(45678)");
+            Console.WriteLine(myHashMap.Get(45678));
+            Console.WriteLine();
+
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+
+            Console.WriteLine("*************************************************");
+            Console.WriteLine();
+            Console.WriteLine("What if we add 2 entries to the hash table that map to the same location?");
+            Console.WriteLine("myHashMap.Put(26189, \" First Entry\")");
+            myHashMap.Put(26189, "FirstEntry");
+            Console.WriteLine("myHashMap.Put(26189, \" Second Entry\")");
+            myHashMap.Put(26189, "Second Entry");
+
+            Console.WriteLine("Re print the hash table and arrays");
+            Console.WriteLine(myHashMap.ToString());
+            Console.WriteLine(myHashMap.ShowArrays());
         }
     }
 }
